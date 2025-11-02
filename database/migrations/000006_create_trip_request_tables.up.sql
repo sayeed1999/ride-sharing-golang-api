9397ddf6495e967444b473b5c3ed5d73e7a59467
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2025-11-02 14:23:48 +0600

    fix: make status int in trip request

--- database/migrations/000006_create_trip_request_tables.up.sql
+++ database/migrations/000006_create_trip_request_tables.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS "trip"."trip_requests" (
     "customer_id" INTEGER NOT NULL,
     "origin" VARCHAR(255) NOT NULL,
     "destination" VARCHAR(255) NOT NULL,
-    "status" VARCHAR(50) NOT NULL,
+    "status" INTEGER NOT NULL,
     "created_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY ("customer_id") REFERENCES "trip"."customers"("id")
