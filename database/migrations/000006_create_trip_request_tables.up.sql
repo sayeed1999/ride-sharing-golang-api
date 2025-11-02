e51a5ffd8716c19485ed84ac285590ae628f5b21
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2025-11-02 13:12:01 +0600

    feat: add migration scripts to generate trip request entity

---
+++ database/migrations/000006_create_trip_request_tables.up.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE IF NOT EXISTS "trip_requests" (
+    "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    "customer_id" UUID NOT NULL,
+    "origin" VARCHAR(255) NOT NULL,
+    "destination" VARCHAR(255) NOT NULL,
+    "status" VARCHAR(50) NOT NULL,
+    "created_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY ("customer_id") REFERENCES "customers"("id")
+);
