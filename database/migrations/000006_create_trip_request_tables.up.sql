011ce738dbaf85b408efc15e6c87567719a6c5a8
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2025-11-02 13:54:59 +0600

    fix: fix foreign key issue

--- database/migrations/000006_create_trip_request_tables.up.sql
+++ database/migrations/000006_create_trip_request_tables.up.sql
@@ -1,11 +1,11 @@
 
 CREATE TABLE IF NOT EXISTS "trip.trip_requests" (
     "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    "customer_id" UUID NOT NULL,
+    "customer_id" INTEGER NOT NULL,
     "origin" VARCHAR(255) NOT NULL,
     "destination" VARCHAR(255) NOT NULL,
     "status" VARCHAR(50) NOT NULL,
     "created_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY ("customer_id") REFERENCES "trip.customers"("id")
+    FOREIGN KEY ("customer_id") REFERENCES "trip"."customers"("id")
 );
